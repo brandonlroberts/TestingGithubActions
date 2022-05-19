cb78c21329036722c733b041a921cec4374dd667
Brandon Roberts <brandon.roberts@qaclaims.com>
2022-05-19 10:13:42 -0400

    Initial commit

---
+++ TestingGithubActions.Models/Entities/TestingClass.cs
@@ -0,0 +1,8 @@
+﻿namespace TestingGithubActions.Models.Entities
+{
+    public class TestingClass
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
